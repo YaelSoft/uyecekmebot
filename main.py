a7a24696a35a2420ca85274215f9d0cf7ab0ad42
YaelSoft <atayasin3344@gmail.com>
2025-12-15 21:27:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from flask import Flask
 # --- AYARLAR ---
 API_ID = 37107052
 API_HASH = "afc7a787cbde453b8fc7134383658b30"
-BOT_TOKEN = "BURAYA_BOT_FATHER_TOKENINI_YAZ"
-SESSION_STRING = "BURAYA_SESSION_STRING_YAZ"
+BOT_TOKEN = "7839067076:AAHgC6C-mzQegzVVHLmkVH08vu-jkTBaQlI"
+SESSION_STRING = "BAI2NWwAj6zZYFPYXXWDK2fNcBeZYSn7qPtcrB-5dQTPyHazeVF7F_fvw2gLMvB5JyB7exqyKcLicCqG5e_o9z46BbsR1lKZCGxaE9xYm3_O_NMI-8ZciOCn6o5VFUMZJnEappc6Py_6eNA2w7kOB-YpYNCOZp5A4cGF_wY_2LWR9UzSbGIeYLMoYokUrYtYTANDNrxG5lX50WtUusyr6_OX1uHsXIRuyeYWNa0qqZJY0A_KuTKKuFBIpn11H0BXf1DSxj1EvpwTM82rh2S1Oq3CfdROQYS0ADvl68-yTf-Sa2EmbeGEa6sXj_-7Z-QjC9lgOiPltG8FMSvw-kWgKRtF2W89igAAAAH4WvQ0AA"
 ADMIN_ID = 8102629232 
 
 # --- FLASK (Sırf Render Kapanmasın Diye) ---
@@ -110,3 +110,4 @@ async def main():
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
+
