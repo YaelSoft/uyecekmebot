ea61060e05462dccfe3362c3905ecb6ae2dea1d8
YaelCode <atayasin3344@gmail.com>
2026-01-26 21:26:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ SESSION_STRING = os.environ.get("SESSION_STRING", "")
 OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
 
 # 🔥🔥🔥 BURAYA LOG KANAL ID'SİNİ ELLE YAZ (Örn: -100123456789) 🔥🔥🔥
-MANUAL_LOG_ID = https://t.me/yaeltanitimxxaa 
+MANUAL_LOG_ID = "https://t.me/yaeltanitimxxaa"
 
 # Öncelik Environment, yoksa Manuel ID
 ENV_LOG = os.environ.get("LOG_CHANNEL", "0")
@@ -703,4 +703,5 @@ if __name__ == '__main__':
     Thread(target=run_web).start()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
+
 
